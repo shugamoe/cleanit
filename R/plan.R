ef734f89fd2462c49934b0442fade61f1c5ef680
Julian McClellan <juliancmcclellan@gmail.com>
2020-02-27 18:47:46 -0600

    Build site. Initial commit of drake+workflowr.

--- R/plan.R
+++ R/plan.R
@@ -1,6 +1,6 @@
 plan  <- drake_plan(
-  aboutWfr = wflow_publish(knitr_in("analysis/about.Rmd"), verbose = T)
-  licenseWfr = wflow_publish(knitr_in("analysis/license.Rmd"), verbose = T)
-  summaryStatsWfr = wflow_publish(knitr_in("analysis/summaryStats.Rmd"), verbose = T)
+  aboutWfr = wflow_publish(knitr_in("analysis/about.Rmd"), verbose = T),
+  licenseWfr = wflow_publish(knitr_in("analysis/license.Rmd"), verbose = T),
+  summaryStatsWfr = wflow_publish(knitr_in("analysis/summaryStats.Rmd"), verbose = T),
   indexWfr = wflow_publish(knitr_in("analysis/index.Rmd"), view = T, verbose = T)
 )
